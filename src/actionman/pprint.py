75540bd5ce3ad3653de62a6d4a47d85da98734fb
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-01-28 06:08:10 +0100

    Update pprint.py

--- src/actionman/pprint.py
+++ src/actionman/pprint.py
@@ -1,8 +1,98 @@
+from typing import Literal as _Literal
 from markitup import sgr as _sgr
 
 
-def entry_github(title: str, details: str, pprint: bool = True) -> str:
+def github_group(title: str, details: str, pprint: bool = True) -> str:
     output = f"::group::{title}\n{details}\n::endgroup::"
+    if pprint:
+        print(output, flush=True)
+    return output
+
+
+def github_group_start(title: str, pprint: bool = True) -> str:
+    output = f"::group::{title}"
+    if pprint:
+        print(output, flush=True)
+    return output
+
+
+def github_group_end(pprint: bool = True) -> str:
+    output = "::endgroup::"
+    if pprint:
+        print(output, flush=True)
+    return output
+
+
+def github_log(
+    message_type: _Literal["debug", "notice", "warning", "error"],
+    message: str,
+    title: str = "",
+    filename: str = "",
+    line_start: int = 0,
+    line_end: int = 0,
+    column_start: int = 0,
+    column_end: int = 0,
+    pprint: bool = True,
+) -> str:
+    """Create either a debug, notice, warning, or error log for GitHub Actions.
+
+    Parameters
+    ----------
+    message_type : {"debug", "notice", "warning", "error"}
+        The type of log to create.
+    message : str
+        The log message.
+    title : str, optional
+        The log title.
+        This is not available (and will be ignored) for the "debug" log type.
+    filename : str, optional
+        Path to a file in the repository to associate the message with.
+        This is not available (and will be ignored) for the "debug" log type.
+    line_start : int, optional
+        The starting line number in the file specified by the 'filename' argument,
+        to associate the message with.
+        This is not available (and will be ignored) for the "debug" log type.
+    line_end : int, optional
+        The ending line number in the file specified by the 'filename' argument,
+        to associate the message with.
+        This is not available (and will be ignored) for the "debug" log type.
+    column_start : int, optional
+        The starting column number in the line specified by the 'line_start' argument,
+        to associate the message with.
+        This is not available (and will be ignored) for the "debug" log type.
+    column_end : int, optional
+        The ending column number in the line specified by the 'line_start' argument,
+        to associate the message with.
+        This is not available (and will be ignored) for the "debug" log type.
+    pprint : bool, default: True
+        Whether to print the notice message to stdout.
+
+    References
+    ----------
+    - [Debug](https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-a-debug-message)
+    - [Notice](https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-a-notice-message)
+    - [Warning](https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-a-warning-message)
+    - [Error](https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-error-message)
+    """
+    args = locals()
+    output = f"::{message_type} "
+    args_added = False
+    if message_type != "debug":
+        for arg_name, github_arg_name in (
+            ("title", "title"),
+            ("filename", "file"),
+            ("line_start", "line"),
+            ("line_end", "endLine"),
+            ("column_start", "col"),
+            ("column_end", "endColumn"),
+        ):
+            if args[arg_name]:
+                output += f"{github_arg_name}={args[arg_name]},"
+                args_added = True
+    else:
+        message = f" {message}"
+    output = output.removesuffix("," if args_added else " ")
+    output += f"::{message}"
     if pprint:
         print(output, flush=True)
     return output
